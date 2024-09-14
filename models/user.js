f421743f417525bd4b627fd4404de082019a769a
Igor Liakh <igorlyah060@gmail.com>
2024-09-14 18:03:39 +0300

    update user model/added field token

--- models/user.js
+++ models/user.js
@@ -11,6 +11,10 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    token: {
+      type: String,
+      default: null,
+    },
   },
   {
     versionKey: false,
