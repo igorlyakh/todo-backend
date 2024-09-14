e43b77070b78d1783047e9b445fe602770fa21da
Igor Liakh <igorlyah060@gmail.com>
2024-09-14 17:22:32 +0300

    added error handler

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((_, res) => {
+  res.status(404).json({ message: 'Route not found!' });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Server error!' } = err;
+  res.status(status).json({ message });
+});
+
 const { PORT = 3000, DB_HOST } = process.env;
 
 mongoose
