7a1547f4db81ba642855a0e5c4947905af0cef2d
Igor Liakh <igorlyah060@gmail.com>
2024-09-20 20:30:08 +0300

    added custom messages for task joi schema

--- schemas/task.js
+++ schemas/task.js
@@ -5,7 +5,10 @@ const taskSchema = Joi.object({
     'string.min': 'Заголовок должен быть не менее {#limit} символов.',
     'any.required': 'Заголовок обязателен для заполнения.',
   }),
-  description: Joi.string().max(32),
+  description: Joi.string().max(32).messages({
+    'string.max': 'Описание не должно быть длиннее чем {#limit} символа!',
+    'string.base': 'Описание должно быть строкой!',
+  }),
   isComplete: Joi.boolean(),
 });
 
