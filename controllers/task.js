cf1a05f4f69abd221b1b395e8e97cbcdbddc8508
Igor Liakh <igorlyah060@gmail.com>
2024-09-20 20:19:02 +0300

    fix createTask

--- controllers/task.js
+++ controllers/task.js
@@ -3,7 +3,7 @@ const { Task } = require('../models');
 class TaskController {
   async createTask(req, res, next) {
     try {
-      const { _is: owner } = req.user;
+      const { _id: owner } = req.user;
       const result = await Task.create({ ...req.body, owner });
       res.status(201).json(result);
     } catch (error) {
