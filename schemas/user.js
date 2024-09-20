81b2edd915624b1bfbbad2f370d03350d616c8e2
Igor Liakh <igorlyah060@gmail.com>
2024-09-20 19:55:29 +0300

    update Joi schema for user

--- schemas/user.js
+++ schemas/user.js
@@ -5,6 +5,10 @@ const userSchema = Joi.object({
     'string.min': 'Email должен быть не менее {#limit} символов.',
     'any.required': 'Email обязателен для заполнения.',
   }),
+  name: Joi.string().min(3).required().messages({
+    'string.min': 'Имя должно быть не менее {#limit} символов.',
+    'any.required': 'Имя обязательно для заполнения.',
+  }),
   password: Joi.string().min(6).required().messages({
     'string.min': 'Пароль должен быть не менее {#limit} символов.',
     'any.required': 'Пароль обязателен для заполнения.',
