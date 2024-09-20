60d6688f717f6443e6c6deaf274fc9de79d867ad
Igor Liakh <igorlyah060@gmail.com>
2024-09-20 19:53:50 +0300

    updated user mongoose model

--- models/user.js
+++ models/user.js
@@ -7,6 +7,10 @@ const userSchema = new Schema(
       required: true,
       unique: true,
     },
+    name: {
+      type: String,
+      require: true,
+    },
     password: {
       type: String,
       required: true,
