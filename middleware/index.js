452989ae52b37086cbfcce842d82da525568a609
Igor Liakh <igorlyah060@gmail.com>
2024-09-21 19:02:10 +0300

    added middleware for chech mongoose id

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,5 @@
 const validateBody = require('./validateBody');
 const isValidToken = require('./isValidToken');
+const isValidId = require('./isValidId');
 
-module.exports = { validateBody, isValidToken };
+module.exports = { validateBody, isValidToken, isValidId };
--- routes/task.js
+++ routes/task.js
@@ -1,12 +1,12 @@
 const { TaskController } = require('../controllers');
-const { isValidToken, validateBody } = require('../middleware');
+const { isValidToken, validateBody, isValidId } = require('../middleware');
 const { taskSchema } = require('../schemas');
 
 const taskRouter = require('express').Router();
 
 taskRouter.post('/', isValidToken, validateBody(taskSchema), TaskController.createTask);
-taskRouter.delete('/:id', isValidToken, TaskController.deleteTask);
+taskRouter.delete('/:id', isValidToken, isValidId, TaskController.deleteTask);
 taskRouter.get('/', isValidToken, TaskController.getAll);
-taskRouter.patch('/:id/complete', isValidToken);
+taskRouter.patch('/:id/complete', isValidToken, isValidId, TaskController.setIsComplete);
 
 module.exports = taskRouter;
