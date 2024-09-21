cd6ae03aba16146b4d9efa0355811f63ace43695
Igor Liakh <igorlyah060@gmail.com>
2024-09-21 19:07:04 +0300

    fix middleware idValidId

--- middleware/isValidId.js
+++ middleware/isValidId.js
@@ -4,7 +4,7 @@ const { HttpError } = require('../helpers');
 const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    next(HttpError(400, `${id} это не существующий id!`));
+    next(HttpError(400, `${id} это несуществующий id!`));
   }
   next();
 };
