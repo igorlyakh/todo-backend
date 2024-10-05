6731eae30860b142425e1406fa62ecf26ac71dd5
Igor Liakh <igorlyah060@gmail.com>
2024-10-05 19:15:08 +0300

    updated Token model

--- models/token.js
+++ models/token.js
@@ -1,15 +1,21 @@
 const { Schema, model } = require('mongoose');
 
-const TokenSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
+const TokenSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
   },
-  refreshToken: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 const Token = model('token', TokenSchema);
 
