4d3ce91f74cc0e2aea7f4e446d154f887900a2ca
Igor Liakh <igorlyah060@gmail.com>
2024-10-05 19:04:48 +0300

    added package cookie-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { userRouter, taskRouter } = require('./routes');
 
@@ -9,6 +10,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.use('/api/user', userRouter);
